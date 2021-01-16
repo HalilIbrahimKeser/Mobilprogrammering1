75c00406f5780df5709a86c714618c55dc2a50c1
HalilKeser <hik003@post.uit.no>
2021-01-17 00:06:57 +0100

    Stasjonær

--- lab1_3/src/main/java/com/example/lab1_3/MainActivityLab1_3.java
+++ lab1_3/src/main/java/com/example/lab1_3/MainActivityLab1_3.java
@@ -3,6 +3,8 @@ package com.example.lab1_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivityLab1_3 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivityLab1_3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lab1_3);
+    }
+
+    public void wrightToast(View view) {
+        Toast.makeText(this, "Din melding!", Toast.LENGTH_LONG).show();
     }
 }
