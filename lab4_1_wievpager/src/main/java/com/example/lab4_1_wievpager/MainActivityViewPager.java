3445b87b93b50feead0af058329003f055119e5f
HalilKeser <hik003@post.uit.no>
2021-02-06 18:14:44 +0100

    Stasjonær

---
+++ lab4_1_wievpager/src/main/java/com/example/lab4_1_wievpager/MainActivityViewPager.java
@@ -0,0 +1,14 @@
+package com.example.lab4_1_wievpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityViewPager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view_pager);
+    }
+}
