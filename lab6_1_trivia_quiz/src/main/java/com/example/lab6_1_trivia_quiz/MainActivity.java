6b56a6da018eef98005738b1c46e0aa6eabf444a
Jørgen <jorgen.rypdal@gmail.com>
2021-02-24 14:05:37 +0100

    first commit

--- lab6_1_trivia_quiz/src/main/java/com/example/lab6_1_trivia_quiz/MainActivity.java
+++ lab6_1_trivia_quiz/src/main/java/com/example/lab6_1_trivia_quiz/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.lab6_1_trivia_quiz;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar quizToolbar = findViewById(R.id.quizToolbar);
+        setSupportActionBar(quizToolbar);
+    }
+
+    public void settingsActivity(MenuItem item) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
