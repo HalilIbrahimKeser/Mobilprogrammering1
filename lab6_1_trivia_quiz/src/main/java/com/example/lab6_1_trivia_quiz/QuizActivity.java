bb6dcc683810b70568a5e07cefc4a6c66a8ca28a
Jørgen <jorgen.rypdal@gmail.com>
2021-03-09 18:49:03 +0100

    gjorde om på main, til å starte en ny aktivitet når du klikker på knapp

--- lab6_1_trivia_quiz/src/main/java/com/example/lab6_1_trivia_quiz/QuizActivity.java
+++ lab6_1_trivia_quiz/src/main/java/com/example/lab6_1_trivia_quiz/QuizActivity.java
@@ -6,34 +6,44 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.lab6_1_trivia_quiz.fragments.ResultsFragment;
+import com.example.lab6_1_trivia_quiz.viewmodel.myViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class QuizActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+import java.util.List;
+
+public class QuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-        bottomNav.setOnNavigationItemSelectedListener(this);
+        myViewModel myViewModel = new ViewModelProvider(this).get(myViewModel.class);
+        myViewModel.getQuiz("10","10", "easy","multiple").observe(this, quizData -> {
+
+            List quizDataResults = quizData.getResults();
+        });
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        Fragment selectedFragment = null;
-        switch (item.getItemId()) {
-            case R.id.hjem:
-                //selectedFragment = ResultsFragment.newInstance;
-                break;
-        }
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container, selectedFragment)
-                //.addToBackStack(null)
-                .commit();
-        return true;
+    public void replaceFragmentWidth(Fragment newFragment, boolean addTobackStack) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (addTobackStack)
+            fragmentManager
+                    .beginTransaction()
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .add(R.id.fragment_container, newFragment)
+                    .commit();
+        else
+            fragmentManager
+                    .beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container, newFragment)
+                    .commit();
     }
+
 }
