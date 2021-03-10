6973155e84ef77a9d12bd01c3747ba6fd2c244ad
Jørgen <jorgen.rypdal@gmail.com>
2021-03-10 19:26:40 +0100

    Fått på plass at når du velger i settings, bygges det opp api kall.

--- lab6_1_trivia_quiz/src/main/java/com/example/lab6_1_trivia_quiz/QuizActivity.java
+++ lab6_1_trivia_quiz/src/main/java/com/example/lab6_1_trivia_quiz/QuizActivity.java
@@ -1,5 +1,7 @@
 package com.example.lab6_1_trivia_quiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -8,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.PreferenceManager;
 
 import com.example.lab6_1_trivia_quiz.models.Question;
 import com.example.lab6_1_trivia_quiz.repository.myRepository;
@@ -15,17 +18,22 @@ import com.example.lab6_1_trivia_quiz.viewmodel.myViewModel;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class QuizActivity extends AppCompatActivity {
     private myRepository myRepo;
+    Map<String, ?> sharePrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
-        myViewModel myViewModel = new ViewModelProvider(this).get(myViewModel.class);
-        myViewModel.getQuiz("10","10", "easy","multiple").observe(this, quizData -> {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Map<String, ?> sharedPrefs = sharedPreferences.getAll();
+                myViewModel myViewModel = new ViewModelProvider(this).get(myViewModel.class);
+        myViewModel.getQuiz(String.valueOf(sharedPrefs.get("num")), String.valueOf(sharedPrefs.get("category")), String.valueOf(sharedPrefs.get("diff")), String.valueOf(sharedPrefs.get("type"))).observe(this, quizData -> {
+            //List<Question> questions = quizData.getResults();
         });
 
         String path = this.getFilesDir().getAbsolutePath()+"/running_quiz.json";
